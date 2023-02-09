55937345fd657c1e2dd73376ca9f2a8ceaaaa51f
shunu <shunmugam@gmail.com>
2023-02-09 15:29:22 +0530

    clean

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.get("/",(rep,res) =>{
 
 
 
-console.log("Version hand signed msg")
+console.log("Version clean version")
 app.listen(5000, () => console.log('Running on port 5000'));
 
 //curl --request POST --url "https://stripeexperiments.onrender.com/webhook" --header "Content-Type: application/json" --data "{\"param1\":\"param1\",\"param2\":\"two\"}"
